18070ee464db0c874df08db1a67184d6b9c5ab98
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2017-05-13 10:37:48 -0500

    Removed dependency on underscore (indexOf instead of _.contains)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function(environments, status) {
   environments = environments || ['production'];
   status = status || 302;
   return function(req, res, next) {
-    if (_.contains(environments, process.env.NODE_ENV)) {
+    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
       if (req.headers['x-forwarded-proto'] != 'https') {
         res.redirect(status, 'https://' + req.hostname + req.originalUrl);
       }
