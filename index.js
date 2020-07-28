49b05b8714a9a0671ef82d369f3b01f98f1e2f0e
Bailey Stoner <monokrome@monokro.me>
2020-07-27 17:41:47 -0700

    Simplify code

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ module.exports = function(environments, status) {
   environments = environments || ['production'];
   status = status || 302;
   return function(req, res, next) {
-    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        res.redirect(status, 'https://' + req.hostname + req.originalUrl);
-      }
-      else {
-        next();
-      }
-    }
-    else {
+    if (environments.indexOf(process.env.NODE_ENV) >= 0
+        && req.headers['x-forwarded-proto'] !== 'https') {
+      res.redirect(status, 'https://' + req.hostname + req.originalUrl);
+    } else {
       next();
     }
   };
